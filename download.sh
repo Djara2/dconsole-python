6da254efc8c20c2c124e00c812c1907b5c1c7b19
Djara2 <62144343+Djara2@users.noreply.github.com>
2021-08-17 14:23:12 -0500

    Improved download.sh script
    Download.sh is used to install stuff that is needed for the program to run

--- download.sh
+++ download.sh
@@ -9,12 +9,14 @@ then
 	pip3 install pyautogui
 	pip3 install pyperclip
 	sudo apt-get install python3-tk python3-dev
+	pip3 install matplotlib
 else
 	apt install python3-pip
 	apt install w3m
 	pip3 install rich
 	pip3 install pyautogui
 	pip3 install pyperclip
-	sudo apt-get install python3-tk python3-dev
+	apt-get install python3-tk python3-dev
+	pip3 install matplotlib
 fi
 
